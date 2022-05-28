7335c5de0a3e63a6860e4a5ad7b5f0a8af94b094
ebinsmarta <99858529+ebinsmarta@users.noreply.github.com>
2022-05-28 10:23:28 +0700

    Update index.js

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ function main(data) {
   console.log(hasil.hasilLooping);
   console.log(hasil.jumlahPria);
   console.log(hasil.jumlahWanita);
-  console.log(hasil.jumlahWanita);
+  console.log(hasil.komentar);
 
   return hasil;
 }
